8aaacf4bba7e5bc0ac05dd3f2d60687699a7d9e2
vastav-ik <guptavastav9389@gmail.com>
2025-12-05 10:29:04 +0530

    Update user model to ensure unique user instance and enhance proxy logic to include email verification path

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,11 @@ import { NextResponse, NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/signup" || path === "/";
+  const isPublicPath =
+    path === "/login" ||
+    path === "/signup" ||
+    path === "/verify-email" ||
+    path === "/";
 
   const token = request.cookies.get("token")?.value || "";
 
